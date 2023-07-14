f19a377cb1d7c8830264c991ddc149cec602bcec
IhorVintonyak <66689738+IhorVintonyak@users.noreply.github.com>
2023-07-14 20:57:40 +0200

    Buttons function
    Function to choose a player. Functionality of random

--- volleyball/volleyball/MainWindow.xaml.cs
+++ volleyball/volleyball/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using System;
+using System.Reflection.Emit;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace volleyball
 {
@@ -8,6 +14,52 @@ namespace volleyball
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Grid.SetRow(ball, 0);
+            Grid.SetColumn(ball, 0);
+ 
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            Grid.SetRow(ball, 0);
+            Grid.SetColumn(ball, 2);
+        }
+
+
+
+        void response()
+        {
+            
+            Random random = new Random();
+            int RandomNumber = random.Next(0, 2);
+            Grid.SetRow(ball, 5);
+            if (RandomNumber == 0)
+            {
+                Grid.SetColumn(ball, RandomNumber);
+            }
+            else
+            {
+                Grid.SetColumn(ball, 2);
+            }
+            
+        }
+
+        void changeActionToAttack()
+        {
+            txtblNameAction.Text = "Attack";
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            response();
+
+
         }
     }
 }
